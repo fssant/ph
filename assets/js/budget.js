ef7f869bd68db77997f5f07ce20be0eca11b64dd
Filipe Santos <filipssant@gmail.com>
2017-10-27 11:46:32 -0200

    Add budget section

---
+++ assets/js/budget.js
@@ -0,0 +1,12 @@
+$('#lastclean').datepicker({
+   language:"pt-BR",
+   orientation: "bottom"
+});
+
+$('#agepool').datepicker({
+   language:"pt-BR",
+   startView: "months", 
+   minViewMode: "months",
+   orientation: "bottom",
+   format: "mm-yyyy"
+})
