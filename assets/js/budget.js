abff98210510c92c714b8757af6f5d5d55442c9b
Filipe Santos <filipssant@gmail.com>
2017-10-27 15:55:15 -0200

    Add function to calc service price

--- assets/js/budget.js
+++ assets/js/budget.js
@@ -19,3 +19,24 @@ $(function() {
       $(this).removeClass('not-active').siblings().addClass('not-active');
    });
 });
+
+//function to generate budget according to pool specifications
+$( "input[type=radio][name=pool], input[type=radio][name=visit]" ).change(function() {
+   // Input radio-group visual controls
+   var item1 = $("input[type=radio][name=pool]:checked" ).val();
+   var item2 = $( "input[type=radio][name=visit]:checked" ).val();
+
+   if( item1 == 1 && item2 == 3){
+      $('#valor').val('200,00');
+   }else if(item1 == 1 && item2 == 4){
+      $('#valor').val('320,00');
+   }else if(item1 == 2 && item2 == 3){
+      $('#valor').val('250,00');
+   }else if(item1 == 2 && item2 == 4){
+      $('#valor').val('400,00');
+   }else{
+      $('#valor').val('');
+   }
+
+});
+
